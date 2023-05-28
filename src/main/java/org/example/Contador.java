daefc75f75b9a9ad08b339d2f60ed9e6668b42ec
DLucMv <dlucasmv@gmail.com>
2023-05-28 20:03:11 -0300

    ParametrosInvalidosException finalizada

--- src/main/java/org/example/Contador.java
+++ src/main/java/org/example/Contador.java
@@ -5,6 +5,30 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero: ");
+        int parametro1 = sc.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int parametro2= sc.nextInt();
+
+        try{
+            contar(parametro1, parametro2);
+        }catch (ParametrosInvalidosException pie){
+            pie.printStackTrace();
+            System.err.println(pie.getMsg());
+        }
+
+    }
+
+    static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException{
+        if(parametro2 < parametro1)
+            throw new ParametrosInvalidosException();
+
+        int contador = parametro2 - parametro1;
+        for (int i = 0; i < contador ; i++) {
+            System.out.println("Imprimindo o número " + (i+1));
+        }
     }
 
 }
--- src/main/java/org/example/ParametrosInvalidosException.java
+++ src/main/java/org/example/ParametrosInvalidosException.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class ParametrosInvalidosException extends Exception{
+
+    String msg = "parâmetro1 não pode ser maior que parâmetro2";
+
+    public ParametrosInvalidosException(){
+    }
+
+    public String getMsg() {
+        return msg;
+    }
 }
